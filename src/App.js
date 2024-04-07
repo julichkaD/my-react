17f71d1a673093059c181da21f3c50164ea75e82
Julia Darwish <darwishjulia4@gmail.com>
2024-04-07 16:51:45 -0400

    Update App routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import RegisterPage from './components/RegisterPage';
+import WelcomePage from './components/WelcomePage';
+import AnotherPage from './components/AnotherPage';
 import './index.css';
 
 function App() {
   return (
     <div className="App">
-
+      <Router>
+        <Routes>
+          <Route path="/" element={<RegisterPage />} />
+          <Route path="/welcome-page" element={<WelcomePage />} />
+          <Route path="/another-page" element={<AnotherPage/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
