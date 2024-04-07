623816c36ca8e44e46213720b86ebe3d7a0cf7ef
Julia Darwish <darwishjulia4@gmail.com>
2024-04-07 16:51:30 -0400

    Create WelcomePage

---
+++ src/components/WelcomePage.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import Button from "@mui/material/Button";
+
+
+const WelcomePage = (props) => {
+    const imageUrl =
+      "https://www.pinclipart.com/picdir/big/88-886440_emoji-feliz-png-emoticon-smile-clipart.png"; 
+       const navigate = useNavigate();
+  return (
+    <div>
+      <h1 id="welcome-page-header">
+        Hello and Welcome dear {props.username}!!
+      </h1>
+      <img
+        src={imageUrl}
+        alt="background"
+        width="500"
+        height="450"
+        id="smile"
+      />
+      <Button id="welcome-btn" onClick={() => navigate("/another-page")}>Click me!</Button>
+    </div>
+  );
+};
+
+export default WelcomePage;
