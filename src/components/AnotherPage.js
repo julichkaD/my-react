6493f58dfed8391aa513a6ec8891e42ffa747906
Julia Darwish <darwishjulia4@gmail.com>
2024-04-07 16:49:48 -0400

    Create AnotherPage component

---
+++ src/components/AnotherPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AnotherPage = () => {
+  return (
+    <div>
+        <h1>HELLO,</h1>
+      <h1>AND I AM ANOTHER PAGE 🤪 😜</h1>
+    </div>
+  )
+}
+
+export default AnotherPage
