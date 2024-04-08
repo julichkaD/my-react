e8c0325a849d1e852ac36992bc73e98aae089f3e
Julia Darwish <darwishjulia4@gmail.com>
2024-04-07 22:21:57 -0400

    Update WelcomePage

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -2,15 +2,14 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import Button from "@mui/material/Button";
 
-
 const WelcomePage = (props) => {
-    const imageUrl =
-      "https://www.pinclipart.com/picdir/big/88-886440_emoji-feliz-png-emoticon-smile-clipart.png"; 
-       const navigate = useNavigate();
+  const imageUrl =
+    "https://www.pinclipart.com/picdir/big/88-886440_emoji-feliz-png-emoticon-smile-clipart.png";
+  const navigate = useNavigate();
   return (
     <div>
       <h1 id="welcome-page-header">
-        Hello and Welcome dear {props.username}!!
+        Hello and Welcome dear {props.username.toUpperCase()}!!
       </h1>
       <img
         src={imageUrl}
@@ -19,7 +18,9 @@ const WelcomePage = (props) => {
         height="450"
         id="smile"
       />
-      <Button id="welcome-btn" onClick={() => navigate("/another-page")}>Click me!</Button>
+      <Button id="welcome-btn" onClick={() => navigate("/another-page")}>
+        Click me!
+      </Button>
     </div>
   );
 };
